3f23316d24076830e85c3db49d61310e5ef68624
windx-foobar <bezalkogoln1ycoder@gmail.com>
2021-08-11 03:06:49 +0300

    добавлено получение categoriesEId и categories

--- app/Transformers/ProductFromFileTransformer.php
+++ app/Transformers/ProductFromFileTransformer.php
@@ -12,10 +12,12 @@ class ProductFromFileTransformer extends TransformerAbstract {
       $this->data = $data;
 
       return [
+         'id' => isset($this->data['id']) ? (int) $this->data['id'] : null,
          'title' => $this->data['title'],
          'eId' => $this->getEId(),
          'price' => $this->data['price'],
-         'categories' => $this->getCategories()
+         'categories' => $this->getCategories(),
+         'categoriesEId' => $this->getCategoriesEId()
       ];
    }
 
@@ -28,14 +30,37 @@ class ProductFromFileTransformer extends TransformerAbstract {
          )->last();
    }
 
+   private function getCategoriesEId() {
+      return collect($this->data)
+         ->filter(
+            function ($_, $key) {
+               $key = strtolower($key);
+
+               return $this->checkCategoriesEId($key);
+            }
+         )->last();
+   }
+
    private function getCategories() {
       return collect($this->data)
          ->filter(
             function ($_, $key) {
                $key = strtolower($key);
 
-               return Str::startsWith($key, 'categor');
+               return $this->checkCategoriesId($key);
             }
          )->last();
+   }
+
+   private function checkCategories($key) {
+      return Str::startsWith($key, 'categor');
+   }
+
+   private function checkCategoriesId($key) {
+      return $this->checkCategories($key) && !Str::endsWith($key, 'eid');
+   }
+
+   private function checkCategoriesEId($key) {
+      return $this->checkCategories($key) && Str::endsWith($key, 'eid');
    }
 }
