5c9891585878c95bc90d166fd02dac15a4efe0d1
windx-foobar <bezalkogoln1ycoder@gmail.com>
2021-08-11 03:07:49 +0300

    переделано обновление, вынесены в свойства данные и категории, при обновлении добавлен диспатч эвента с отправкой на почту

--- app/Services/ProductsService.php
+++ app/Services/ProductsService.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Services;
 
 use App\Events\CreateEntity;
@@ -13,19 +15,31 @@ use Exception;
  */
 class ProductsService {
    /**
+    * @var \Illuminate\Support\Collection
+    */
+   private $data;
+
+   /**
+    * @var \Illuminate\Database\Eloquent\Collection | Category[] | null
+    */
+   private $categories = null;
+
+   /**
     * @throws Exception
     */
    public function create(array $data): Product {
+      $this->data = collect($data);
+
       try {
          ProductValidator::validate($data)->validate();
 
          $product = new Product();
 
-         $product->title = $data['title'];
-         $product->price = $data['price'];
-         $product->eId = $data['eId'] ?? null;
+         $product->title = $this->data->get('title');
+         $product->price = $this->data->get('price');
+         $product->eId = $this->data->get('eId');
 
-         $categories = Category::find($data['categories'] ?? []);
+         $categories = $this->setCategories()->categories;
 
          if ($product->save()) {
             $product->categories()->attach($categories);
@@ -48,10 +62,12 @@ class ProductsService {
       try {
          ProductValidator::validate($data, ['update' => true])->validate();
 
-         $categories = Category::find($data['categories']);
+         $categories = $this->setCategories()->categories;
          $product->categories()->attach($categories);
 
          if ($product->update($data)) {
+            \Event::dispatch(new CreateEntity($product, true));
+
             return $product;
          }
 
@@ -59,5 +75,27 @@ class ProductsService {
       } catch (Exception $e) {
          throw $e;
       }
+   }
+
+   private function setCategories(): self {
+      $categoriesIds = $this->data->get('categories');
+      $categoriesEIds = $this->data->get('categoriesEId');
+
+      if ($categoriesIds) {
+         $this->categories = Category::find($categoriesIds);
+      }
+
+      if ($categoriesEIds) {
+         $categories = Category::findByEId($categoriesEIds);
+
+         if ($this->categories) {
+            $this->categories->merge($categories);
+            return $this;
+         }
+
+         $this->categories = $categories;
+      }
+
+      return $this;
    }
 }
